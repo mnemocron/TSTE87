926509c6c3fbf21b78cd0c0924759bf66cbbff0a
simbu448 <simbu448@sutudent.liu.se>
2022-04-08 09:58:59 +0200

    lab 1 almost done

--- labs/lab1/introduction.m
+++ labs/lab1/introduction.m
@@ -202,21 +202,21 @@ sfg = [];
 sfg = addoperand(sfg, 'in', 1, 1);
 sfg = addoperand(sfg, 'twoport', 1, [1, 4], [2, 3], a0, 'symmetric');
 sfg = addoperand(sfg, 'delay', 1, 3, 4);
-sfg = addoperand(sfg, 'twoport', 2, [ 1,17], [11,13], a1, 'symmetric');
-sfg = addoperand(sfg, 'twoport', 3, [13,15], [16,14], a2, 'symmetric');
-sfg = addoperand(sfg, 'delay', 2, 14, 15);
-sfg = addoperand(sfg, 'delay', 3, 16, 17);
+sfg = addoperand(sfg, 'twoport', 2, [ 1,16], [11,12], a1, 'symmetric');
+sfg = addoperand(sfg, 'twoport', 3, [12,14], [15,13], a2, 'symmetric');
+sfg = addoperand(sfg, 'delay', 2, 13, 14);
+sfg = addoperand(sfg, 'delay', 3, 15, 16);
 sfg = addoperand(sfg, 'twoport', 4, [ 2,10], [ 5, 6], a3, 'symmetric');
 sfg = addoperand(sfg, 'twoport', 5, [ 6, 8], [ 9, 7], a4, 'symmetric');
 sfg = addoperand(sfg, 'delay', 4,  9, 10);
 sfg = addoperand(sfg, 'delay', 5,  7,  8);
-sfg = addoperand(sfg, 'twoport', 6, [11,22], [23,18], a5, 'symmetric');
-sfg = addoperand(sfg, 'twoport', 7, [18,20], [21,19], a6, 'symmetric');
-sfg = addoperand(sfg, 'delay', 6, 21, 22);
-sfg = addoperand(sfg, 'delay', 7, 19, 20);
-sfg = addoperand(sfg, 'add', 1, [5 23], 24);
-sfg = addoperand(sfg, 'constmult', 1, 24,25, 0.5);
-sfg = addoperand(sfg, 'out', 1, 25);
+sfg = addoperand(sfg, 'twoport', 6, [11,21], [22,17], a5, 'symmetric');
+sfg = addoperand(sfg, 'twoport', 7, [17,19], [20,18], a6, 'symmetric');
+sfg = addoperand(sfg, 'delay', 6, 20, 21);
+sfg = addoperand(sfg, 'delay', 7, 18, 19);
+sfg = addoperand(sfg, 'add', 1, [5 22], 23);
+sfg = addoperand(sfg, 'constmult', 1, 23,24, 0.5);
+sfg = addoperand(sfg, 'out', 1, 24);
 
 coeff = [a0,a1,a2,a3,a4,a5,a6];
 sfg_lwdf = sfg_LWDF(coeff);
@@ -233,12 +233,20 @@ plot(out2); hold on; plot(out1); legend
 %% 4 b)
 % Simulate the filter using an impulse and check the values of all 
 % interesting nodes. Comments?
+
+% Interesting nodes = outputs of adders
+% the internal adder is not of interest because it is a subtraction
+% (is this correct? it could still overflow, right?)
 N = 256;
 random = 2*rand(1, 1024)-1;
 impulse = [1, zeros(1,N-1)];
 [output_org, outputids, registers, regids, nodes, nodeids] = simulate(sfg, impulse);
 
-node_sum = sum(abs(nodes'))
+node_sum = sum(abs(nodes'));
+
+interest = [2,3,5,6,7,9,11,12,13,15,17,22,18,20];
+display(node_sum(interest))
+% all nodes are unsafe from overflowing because all are >1.0
 
 %% 4 c)
 % Plot the phase-response of the two allpass branches in the same plot.
@@ -246,15 +254,70 @@ node_sum = sum(abs(nodes'))
 % which can be obtained as output from freqz. Alternatively, phasez can be 
 % used to directly obtain the phase response from the impulse response.
 
+resp_upper = nodes( 5,:);
+resp_lower = nodes(22,:);
+
+%hold on; grid on
+%stem(resp_upper)
+%stem(resp_lower)
+%legend(["upper allpass", "lower allpass"])
+
+w_up = phasez(resp_upper);
+w_lo = phasez(resp_lower);
+[h,w] = freqz(nodes(24,:));
+
+ang = linspace(0,1,512);
+
+subplot(1,3,1)
+hold on; grid on
+plot(ang, w_up./pi.*180)
+plot(ang, w_lo./pi.*180)
+title("Phase Response")
+xlabel("Normalized Frequency (x \pi rad/sample)")
+ylabel("Phase (deg)")
+legend(["upper allpass", "lower allpass"])
+
+subplot(1,3,2)
+plot(w/pi, db(h))
+grid on
+title("Magnitude Response")
+legend("y(n)")
+xlabel("Normalized Frequency (x \pi rad/sample)")
+ylabel("Magnitude (dB)")
+
+subplot(1,3,3)
+plot(ang, (w_up-w_lo)./pi.*180)
+grid on
+title("Difference in Phase Response")
+xlabel("Normalized Frequency (x \pi rad/sample)")
+ylabel("Phase (deg)")
+
+
 %% 4 d)
 % How large is the difference in phase in the pass- and stopband?
 
+% exactly 180 degrees or pi radians, the sum of the two allpasses cancel
+% each other out
 
 %% 4 e)
 % Scale the filter using L2-scaling (only use power-of-two scaling 
 % coefficients). Indicate where you introduce scaling. Note that all four 
 % sections should have as large dynamic range as possible.
 
+sfg_s = sfg;
+sfg_s = insertoperand(sfg_s, 'constmult', 2, 1, 2^-3); % node 2 and 15 overflows
+sfg_s = insertoperand(sfg_s, 'constmult', 3, 24, 2^3);    % correct node 2 and 15 rescale
+
+sfg_s = insertoperand(sfg_s, 'constmult', 4, 2, 0.5);  % node 9 overflows
+sfg_s = insertoperand(sfg_s, 'constmult', 5, 5, 2);    % correct node 9 rescale
+
+sfg_s = insertoperand(sfg_s, 'constmult', 6, 11, 2^-2);  % node 17 overflows
+sfg_s = insertoperand(sfg_s, 'constmult', 7, 22, 2^2);    % correct node 17 rescale
+
+errors = checknodes(sfg)
+
+
+%dotsfgplot(sfg_s, 'eps')
 
 %% 4 f)
 % Simulate the scaled filter. What is the value of the L2-sum in the 
@@ -262,7 +325,7 @@ node_sum = sum(abs(nodes'))
 % the filter function is not changed?
 
 [output_org, outputids, registers, regids, nodes, nodeids] = simulate(sfg_s, impulse);
-node_sum = sum(abs(nodes'))
+node_sum = sqrt(sum(abs(nodes').^2))  % L2 norm!
 
 % verify filter function
 out1 = impulseresponse(sfg, N);
@@ -278,12 +341,93 @@ plot(w2/pi,db(h2))
 %% 4 g)
 % Simulate the scaled filter with a random input. Comments? Can overflow 
 % occur in any node?
-[output_org, outputids, registers, regids, nodes, nodeids] = simulate(sfg_s, random);
+
+y0 = simulate(sfg,   impulse);
+y1 = simulate(sfg,   impulse, 1, [], [], [1,15]);
+y2 = simulate(sfg_s, impulse, 1, [], [], [1,15]);
+r1 = simulate(sfg,   random, 1, [], [], [1,15]);
+r2 = simulate(sfg_s, random, 1, [], [], [1,15]);
+
+subplot(2,2,1)
+hold on
+grid on
+stem(y1)
+stem(y2)
+title("impulse response")
+legend(["unscaled (overflows)", "scaled"])
+
+subplot(2,2,2)
+hold on
+grid on
+plot(r1)
+plot(r2)
+title("random signal input")
+legend(["unscaled (overflows)", "scaled"])
+
+subplot(2,2,3)
+grid on
+plot(r1-r2)
+title("output error for random signal input")
+legend(["r_1 - r_2"])
+
+subplot(2,2,4)
+histogram(r1-r2)
+title("error distribution")
+
+figure()
+[h1,w1] = freqz(y0);
+[h2,w2] = freqz(y2);
+
+plot(w1/pi, db(h1));
+hold on
+grid on
+plot(w2/pi,db(h2))
+title("Magnitude Response")
+legend(["unscaled float64", "scaled sfix16"])
 
 %% 4 h)
 % Introduce a pipelining delay in each branch and compare the output with 
 % the non-pipelined SFG. Comments?
 
+sfg_p = sfg_s;
+sfg_p = insertoperand(sfg_p, 'delay', 8, 2); 
+sfg_p = insertoperand(sfg_p, 'delay', 9, 11); 
+
+errors = checknodes(sfg)
+
+y0 = simulate(sfg,   impulse);
+y1 = simulate(sfg_s,   impulse, 1, [], [], [1,15]);
+y2 = simulate(sfg_p, impulse, 1, [], [], [1,15]);
+r1 = simulate(sfg_s,   random, 1, [], [], [1,15]);
+r2 = simulate(sfg_p, random, 1, [], [], [1,15]);
+
+subplot(1,2,1)
+[h0,w0] = freqz(y0);
+[h1,w1] = freqz(y1);
+[h2,w2] = freqz(y2);
+
+w_0 = phasez(y0);
+w_s = phasez(y1);
+w_p = phasez(y2);
+
+plot(w0/pi, db(h0));
+hold on; grid on
+plot(w1/pi,db(h1))
+plot(w2/pi,db(h2))
+title("Magnitude Response")
+legend(["unscaled float64", "scaled sfix16", "scaled & pipelined sfix16"])
+
+subplot(1,2,2)
+hold on; grid on
+plot(ang, w_0./pi.*180)
+plot(ang, w_s./pi.*180)
+plot(ang, w_p./pi.*180)
+title("Phase Response")
+xlabel("Normalized Frequency (x \pi rad/sample)")
+ylabel("Phase (deg)")
+legend(["unscaled float64", "scaled sfix16", "scaled & pipelined sfix16"])
+
+% Todo: comments? is this even correct?
 
 %% 4 i)
 % Plot the precedence graph for the pipelined SFG.
