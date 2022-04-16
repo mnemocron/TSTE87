95ee6a87de8e661e173a35c53a588e93a6780708
Simon Burkhardt <simon@simonmartin.ch>
2022-04-16 11:54:56 +0200

    lab2 task2 now working

--- labs/lab2/interpolator.m
+++ labs/lab2/interpolator.m
@@ -94,12 +94,14 @@ input3 = upsample(output2,2);
 [h2,w2]   = freqz(output2);
 [h3,w3]   = freqz(output3);
 
+figure
 plot(w2/pi, db(h2));
 hold on
 grid on
 plot(w3/pi,db(h3))
 legend(["1x upsampled", "2x upsampled"])
 ylim([-200 50]);
+title("Task 1 b) frequency response")
 % legend(["H_{AP} + H_0", "H_{AP} + H_0 + H_0"])
 
 %% 1 c) What is the passband edge for these two cases?
@@ -130,12 +132,14 @@ input3q = upsample(output2q,2);
 [h2q,w2q]   = freqz(output2q);
 [h3q,w3q]   = freqz(output3q);
 
+figure
 plot(w2/pi, db(h2), '--');
 hold on
 grid on
 plot(w3/pi, db(h3), '--');
 plot(w2q/pi,db(h2q));
 plot(w3q/pi,db(h3q));
+title("Task 1 e) effect of quantization on magn response")
 legend(["1x upsampled", "2x upsampled", "1x upsampled (quantized)", "2x upsampled (quantized)"])
 % legend(["H_{AP} + H_0", "H_{AP} + H_0 + H_0"])
 
@@ -162,14 +166,15 @@ input3r = upsample(output2r,2);
 add_ins = [10 22];
 del_ids = [2 4 5 8 9];
 
+figure
 subplot(1,2,1)
 stem(nodes3(add_ins, :)')
-title("adder inputs")
+title("Task 1g) plot adder inputs")
 grid on
 
 subplot(1,2,2)
 stem(nodes3(del_ids(1), :)')
-title("one delay stage")
+title("Task 1g) plot one delay stage")
 grid on
 
 % Comment
@@ -188,36 +193,45 @@ grid on
 % Remember to use the quantized coeﬃcients.
 
 % Create the SFG. This can, e.g., be done by modifying the SFG:s used in Task 1.
+% Create new SFG, note that:
+% - delay v0 is missing
+% - adder at the output is missing
+% - instead, 2 new outputs are present
 
-% cascading H_A and the first H_0
-% the second H_0 cannot be cascaded, 
-% so run 2 simulation steps instead of 3
-clc
-sfgc_q = cascadesfg(sfga_q, sfgb_q);
-sfgc_q = removeoperand(sfgc_q, 'out', 1);
-sfgc_q = removeoperand(sfgc_q, 'add', 1);
-sfgc_q = addoperand(sfgc_q, 'out', 1, 32);
-sfgc_q = addoperand(sfgc_q, 'out', 2, 44);
-errors = checknodes(sfgc_q)
+sfgb_c = [];
 
-sfgd_q = sfgb_q;
-sfgd_q = removeoperand(sfgd_q, 'out', 1);
-sfgd_q = removeoperand(sfgd_q, 'add', 1);
-sfgd_q = addoperand(sfgd_q, 'out', 1, 10);
-sfgd_q = addoperand(sfgd_q, 'out', 2, 22);
-errors = checknodes(sfgd_q)
+sfgb_c = addoperand(sfgb_c, 'in', 1, 1);
+sfgb_c = addoperand(sfgb_c, 'twoport', 1, [1 3], [4 2], a3, 'symmetric');
+sfgb_c = addoperand(sfgb_c, 'twoport', 2, [4 6], [7 5], a7, 'symmetric');
+sfgb_c = addoperand(sfgb_c, 'twoport', 3, [1 9], [10 8], a1, 'symmetric');
+sfgb_c = addoperand(sfgb_c, 'twoport', 4, [10 12], [13 11], a5, 'symmetric');
+sfgb_c = addoperand(sfgb_c, 'twoport', 5, [13 15], [16 14], a9, 'symmetric');
+sfgb_c = addoperand(sfgb_c, 'delay', 1, 2, 3);
+sfgb_c = addoperand(sfgb_c, 'delay', 2, 5, 6);
+sfgb_c = addoperand(sfgb_c, 'delay', 3, 8, 9);
+sfgb_c = addoperand(sfgb_c, 'delay', 4, 11, 12);
+sfgb_c = addoperand(sfgb_c, 'delay', 5, 14, 15);
+sfgb_c = addoperand(sfgb_c, 'out', 1, 7);
+sfgb_c = addoperand(sfgb_c, 'out', 2, 16);
+
+
+% plotprecedence(sfgb2);
 
 %% b)
 % Simulate the impulse response and using your reference signal to validate 
 % that the ﬁlter has the same function as the ﬁrst design iteration. 
 % To interleave signals you can use reshape. Note that it is important to 
 % take the ﬁrst value from the correct branch. 
-clc
-[output1c,outputids1,registers1,registerids1,nodes1,nodeids1] = simulate(sfgc_q, random);
-input2c = reshape([output1c(1,:);output1c(2,:)], 1, 2*N);
-[output2c,outputids2,registers2,registerids2,nodes2,nodeids2] = simulate(sfgc_q, input2c);
-output3c = reshape([output2c(1,:);output2c(2,:)], 1, 2*2*N);
 
+% "cascading" is still done by using 3 simulation steps for the 3 filters
+clc
+[output1,outputids1,registers1,registerids1,nodes1,nodeids1] = simulate(sfga_q, random);
+[output2,outputids2,registers2,registerids2,nodes2,nodeids2] = simulate(sfgb_c, output1);
+% instead of upsampling, do interleaving of adder inputs:
+input3 = reshape([output2(2,:);output2(1,:)],1,2*length(output2(2,:)));
+[output3,outputids3,registers3,registerids3,nodes3,nodeids3] = simulate(sfgb_c, input3);
+% instead of upsampling, do interleaving of adder inputs:
+output4 = reshape([output3(2,:);output3(1,:)],1,2*length(output3(2,:)));
 
 % out_ids = [ find(nodeids1==32), find(nodeids1==44) ];
 % input3c = reshape([nodes1(out_ids(1),:);nodes1(out_ids(2),:)], 1, 2*N);
@@ -225,109 +239,15 @@ output3c = reshape([output2c(1,:);output2c(2,:)], 1, 2*2*N);
 % out_ids = [ find(nodeids1==10), find(nodeids1==22) ];
 % output5c = reshape([nodes1(out_ids(1),:);nodes1(out_ids(2),:)], 1, 2*2*N);
 
-stem(output3r); hold on;  stem(input2c); stem(output3c)
-%%
-input3c = reshape([output2c(2,:);output2c(1,:)],1,2*length(output2(2,:)));
-% input2c = reshape([nodes1(out_ids(2),:);nodes1(out_ids(1),:)], 1, 2*N);
-[output4c,outputids1,registers1,registerids1,nodes1,nodeids1] = simulate(sfgb_q, input3c);
-
-
-output2c = reshape([nodes1(out_ids(2),:);nodes1(out_ids(1),:)], 1, 2*N);
-
-stem(output2r); hold on; stem(output2c)
+figure
+stem(output3r); 
+hold on;
+grid on;
+stem(output4); 
+legend(["original 2x upsampled", "commutaded 2x upsampled"])
+title("Task 2 b) Simulate commutated SFG")
 
 %%
-
-% second interpolation filter
-[output_org, outputids, registers, regids, nodes, nodeids] = simulate(sfgb_q, resp_dr_1);
-out_ids = [ find(nodeids==10), find(nodeids==22) ];
-
-
-resp_dr_2 = reshape([nodes(out_ids(1),:);nodes(out_ids(2),:)], 1, 4*N);
-
-[h1q,w1q]   = freqz(resp_dr_1);
-[h2q,w2q]   = freqz(resp_dr_2);
-
-plot(w2/pi, db(h2));
-hold on
-grid on
-plot(w2q/pi,db(h2q))
-
-
-
-%%
-% 3 a)
-
-clear all; clc; close all;
-
-N = 128;
-impulse = [1, zeros(1,N-1)];
-random = 2*rand(1,N)-1;
-
-a10 = 0.4573;
-a11 = -0.2098;
-a12 = 0.5695;
-a13 = -0.2123;
-a14 = 0.0952;
-a15 = -0.2258;
-a16 = -0.4490;
-
-a1 = -0.068129;
-a3 = -0.242429;
-a5 = -0.461024;
-a7 = -0.678715;
-a9 = -0.888980;
-
-% quantization
-Wf = 11;
-a10 = round(a10 .* 2^Wf) .* 2^-Wf;
-a11 = round(a11 .* 2^Wf) .* 2^-Wf;
-a12 = round(a12 .* 2^Wf) .* 2^-Wf;
-a13 = round(a13 .* 2^Wf) .* 2^-Wf;
-a14 = round(a14 .* 2^Wf) .* 2^-Wf;
-a15 = round(a15 .* 2^Wf) .* 2^-Wf;
-a16 = round(a16 .* 2^Wf) .* 2^-Wf;
-a1 = round(a1 .* 2^Wf) .* 2^-Wf;
-a3 = round(a3 .* 2^Wf) .* 2^-Wf;
-a5 = round(a5 .* 2^Wf) .* 2^-Wf;
-a7 = round(a7 .* 2^Wf) .* 2^-Wf;
-a9 = round(a9 .* 2^Wf) .* 2^-Wf;
-
-sfga = [];
-
-sfga = addoperand(sfga, 'in', 1, 1);
-sfga = addoperand(sfga, 'twoport', 1, [ 1  3], [ 4  2], a10, 'symmetric');
-sfga = addoperand(sfga, 'twoport', 2, [ 4  9], [10  5], a11, 'symmetric');
-sfga = addoperand(sfga, 'twoport', 3, [ 6  8], [ 9  7], a12, 'symmetric');
-sfga = addoperand(sfga, 'twoport', 4, [10 15], [16 11], a13, 'symmetric');
-sfga = addoperand(sfga, 'twoport', 5, [12 14], [15 13], a14, 'symmetric');
-sfga = addoperand(sfga, 'twoport', 6, [16 21], [22 17], a15, 'symmetric');
-sfga = addoperand(sfga, 'twoport', 7, [18 20], [21 19], a16, 'symmetric');
-sfga = addoperand(sfga, 'delay', 1,  2,  3);
-sfga = addoperand(sfga, 'delay', 2,  5,  6);
-sfga = addoperand(sfga, 'delay', 3,  7,  8);
-sfga = addoperand(sfga, 'delay', 4, 11, 12);
-sfga = addoperand(sfga, 'delay', 5, 13, 14);
-sfga = addoperand(sfga, 'delay', 6, 17, 18);
-sfga = addoperand(sfga, 'delay', 7, 19, 20);
-% sfga = addoperand(sfga, 'out', 1, 22);
-
-% Bireciprocal low-pass filter for interpolator
-% Adaptor coefficients (all even coefficients are zero)
-a1 = -0.068129;
-a3 = -0.242429;
-a5 = -0.461024;
-a7 = -0.678715;
-a9 = -0.888980;
-
-
-
-
-
-
-
-
-
 
 
 
