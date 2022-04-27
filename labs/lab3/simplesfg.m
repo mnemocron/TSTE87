2e41564f1a03a201513a85ce9b239eccf40e1933
Simon Burkhardt <simon@simonmartin.ch>
2022-04-27 12:49:34 +0200

    started lab3

--- labs/lab3/simplesfg.m
+++ labs/lab3/simplesfg.m
@@ -1,3 +1,10 @@
+
+
+% addpath /courses/TSTE87/matlab/
+addpath ../../../newasictoolbox/
+
+%% 
+
 sfg=addoperand([],'in',1,1);
 sfg=addoperand(sfg,'constmult',1,1,2,0.25);
 sfg=addoperand(sfg,'constmult',2,4,5,0.75);
@@ -6,3 +13,30 @@ sfg=addoperand(sfg,'add',2,[2 5],3);
 sfg=addoperand(sfg,'add',3,[6 4],7);
 sfg=addoperand(sfg,'delay',1,3,4);
 sfg=addoperand(sfg,'out',1,7);
+
+timinginfo = getdefaulttiminginfo;
+%% a) 
+% Compute an initial schedule and print it. 
+% Assume latency = execution time = 2 time units.
+
+timinginfo.constmult.latency = 2;
+timinginfo.constmult.executiontime = 2;
+timinginfo.add.latency = 2;
+timinginfo.add.executiontime = 2;
+
+schedule = getinitialschedule(sfg, timinginfo);
+
+plotschedule(schedule)
+
+%% b) Print the possible changes in start times.
+printstarttimes(schedule)
+
+%% c) 
+% Reschedule so that at most one multiplication and one addition are 
+% executed in parallel, with a schedule time of at most six time units. 
+% Note that it is possible to move outputs.
+
+
+
+
+
