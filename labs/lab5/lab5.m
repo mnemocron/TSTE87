4b5b6bced31c531dd85ae5697e6d70f0fd75ed1e
Simon Burkhardt <simon@simonmartin.ch>
2022-05-11 14:26:16 +0200

    added lab5 instructions

--- labs/lab5/lab5.m
+++ labs/lab5/lab5.m
@@ -1,8 +1,11 @@
 
-addpath /courses/TSTE87/matlab/
-% addpath ../../../newasictoolbox/
+% addpath /courses/TSTE87/matlab/
+addpath ../../../newasictoolbox/
 
-%%
+%% Task 1
+% The first task is to generate VHDL descriptions of the memory and 
+% PE controller using information from Laboratory work 4, and simulate 
+% the behavior of these. 
 clear all; clc
 schedule = [
 48	2	NaN	NaN	NaN	NaN	NaN	NaN	NaN	NaN	NaN	NaN	NaN
@@ -34,41 +37,48 @@ schedule = [
 11	22	46	37	55	48	-0.678715000000000	3	0	0	32	15	8
 2	3	54	NaN	NaN	NaN	NaN	NaN	0	NaN	29	0	0];
 
-
-%%
+%% b)
+% Generate VHDL for the timing controller. 
+% Place the file in the working directory.
 
 scheduletime = schedule(1,1);
 generatetimingcontroller(scheduletime);
 
-%%
+%% c)
+% Generate VHDL for the memory controllers, number them 1 and 2. 
+% Place the files in the working directory.
+
 mv = extractmemoryvariables(schedule);
 [mvlist, iv] = memorypartitioning(mv, 1, 1, 1); % 1R 1W 1parallel
 peassignment = getpeassignment(schedule);
-disp(length(mvlist))  % 2 memories
+% disp(length(mvlist))  % 2 memories
 
 cellassignment_1 = leftedgealgorithm(mvlist{1});
 cellassignment_2 = leftedgealgorithm(mvlist{2});
-
-
-%%
-BIT_RESOLUTION = 10;
-
 generatememorycontroller(cellassignment_1, 1);
 generatememorycontroller(cellassignment_2, 2);
+
+%% d) 
+% Generate VHDL for the PE controllers, number them 1, 2, 3, and 4. 
+% Note that the number of bits should be one integer bit plus the number 
+% of fractional bits.
+% Place the files in the working directory.
+
+BIT_RESOLUTION = 10; % as seen inside of the VHDL testbench
 
 generatepecontroller(peassignment{1}, schedule, 1, BIT_RESOLUTION);
 generatepecontroller(peassignment{2}, schedule, 2, BIT_RESOLUTION);
 generatepecontroller(peassignment{3}, schedule, 3, BIT_RESOLUTION);
 generatepecontroller(peassignment{4}, schedule, 4, BIT_RESOLUTION);
 
-
-%%
-clc
-
+%% g) ff.
+close all; clc;
 plotcellassignment(cellassignment_1)
 plotcellassignment(cellassignment_2)
 
 %% Task 2
+%  In this task we will use the allpass filter of the interpolator 
+% filter from laboratory work 2
 clear all;clc;
 
 a10 = 0.4573;
@@ -98,9 +108,21 @@ sfga = addoperand(sfga, 'delay', 6, 17, 18);
 sfga = addoperand(sfga, 'delay', 7, 19, 20);
 sfga = addoperand(sfga, 'out', 1, 22);
 
+%% a) 
+% The latency and execution time are assumed to be equal in this task. 
+% Furthermore, all basic operations (addition, subtraction, 
+% and multiplication) are assumed to have an execution time of 
+% one time unit.
+
+% b) Study the schedule obtained from getinitialschedule using an 
+% execution time corresponding to the critical path of a twoport adaptor. 
+% See previous laboratory works to find the number of basic operations in
+% the critical path.
+
 timinginfo = getdefaulttiminginfo;
 timinginfo.twoport.latency = 3;
 timinginfo.twoport.executiontime = 3;
+% a twoport has a critical path or 1 adder, 1 multiplier and 1 subtractor
 
 schedule = getinitialschedule(sfga, timinginfo);
 
@@ -108,6 +130,11 @@ plotschedule(schedule)
 printstarttimes(schedule)
 
 %% c)
+% Transform the twoport operations to lower level operations using the 
+% command flattensfg. Study the schedule and compare with the previous 
+% schedule where twoport operations was used. Which low level operations 
+% correspond to which twoport operations? What is the required number of
+% processing elements?
 sfg_f = flattensfg(sfga);
 schedule = getinitialschedule(sfg_f, timinginfo);
 
@@ -118,27 +145,38 @@ plotschedule(schedule)
 % 8 add
 
 %% d)
+%  Add the path to the evaluation command in MATLAB. 
+% This can be done by
 addpath /courses/TSTE87/labs/evaluation/
 
 %% e)
+% Study the schedule obtained by evaluation using "infinite" resources. 
+% Compare to the previous schedule. Comments?
 schedule_e = evaluation(sfg_f, [Inf, Inf, Inf], 1,0);
 
 plotschedule(schedule_e)
 % identical to before because resources are not limited
 
 %% f)
+% Study the schedule obtained using resources corresponding to one 
+% twoport operation. Comments?
 schedule_c = evaluation(sfg_f, [1, 2, 1], 1,0);
 
 plotschedule(schedule_c)
 % takes 13 instead of 12 time units
 
-
 %% g)
+%  What is the required overhead hardware, i.e., registers and multiplexers
+% (just count the total number of multiplexer inputs)? 
+% Note that information about this is printed in the MATLAB command window.
+
 % There are 11 registers.
 % There are 15 multiplexers:
 mux_input_count = 2*8 + 3*2 + 4*2 + 5*1 + 6*1 + 7*1
 
 %% h)
+% Pipeline the allpass filter so that the critical path only includes 
+% two twoport operations.
 sfg_p = sfga;
 sfg_p = insertoperand(sfg_p, 'delay', 8, 10);
 sfg_p = insertoperand(sfg_p, 'delay', 9, 16);
@@ -148,6 +186,8 @@ schedule_p = getinitialschedule(sfg_p, timinginfo);
 plotschedule(schedule_p)
 
 %% i)
+% Compare the schedules obtained by getinitialschedule and evaluation 
+% (using resources corresponding to one twoport operation). Comments?
 schedule_p = evaluation(sfg_p, [1, 2, 1], 1,0);
 
 plotschedule(schedule_p)
@@ -155,29 +195,36 @@ plotschedule(schedule_p)
 % 9 instead of 6
 
 %% j)
+% What is the required overhead hardware?
+
 % There are 13 registers.
 % There are 16 multiplexers:
 mux_input_count = 2*7 + 3*4 + 5*2 + 6*2 + 7*1
 
 %% k)
+% Study a schedule obtained using resources corresponding to two twoport 
+% operations. Comments?
 schedule_tp = evaluation(sfg_p, [2, 4, 2], 1,0);
 
 plotschedule(schedule_tp)
 % takes more time than wih infinite resources
 % 7 instead of 6
 
-%% l)
+%% l) 
+% What is the required overhead hardware?
+
 % There are 16 registers.
 % There are 27 multiplexers:
 mux_input_count = 4*4 + 3*5 + 2*18
 
-
 %% m)
-
+% In which of the three schedules for the pipelined filters is the 
+% hardware used most efficiently? 
+% What is the utilization ratio for this case?
 plotschedule(getinitialschedule(sfg_p, timinginfo))
 plotschedule(schedule_p)
 
-% Resource utilization
+% Resource utilization ratio
 
 % original
 % add: 2/6
